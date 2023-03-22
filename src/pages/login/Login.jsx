4a3ef49770a41be2b34d2b29540c1ed9eecd0b23
Sonarpratik <sonarpratik02.ps@gmail.com>
2023-03-22 15:56:46 +0530

    mobile-css

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -23,7 +23,7 @@ const loading=true;
     setLoad("Wait")
     e.preventDefault();
 
-    const response = await fetch("/login", {
+    const response = await fetch("https://userbackend.sonarpratik.repl.co/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
